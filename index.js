99f250f43b5fdfc8543c0f3df7c0c13a0ae3c5c0
pelanguero <pelanguero@gmail.com>
2023-08-28 00:41:16 -0500

    move and mine

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const mineflayer = require('mineflayer')
 const mineflayerViewer = require('prismarine-viewer').mineflayer
+const { pathfinder, Movements, goals: { GoalNear } } = require('mineflayer-pathfinder')
+const Vec3 = require('vec3');
+const RANGE_GOAL = 1
 const bot = mineflayer.createBot({
   host: 'localhost', // minecraft server ip
   username: 'bottest', // minecraft username
@@ -8,16 +11,59 @@ const bot = mineflayer.createBot({
   // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // password: '12345678'        // set if you want to use password-based auth (may be unreliable)
 })
+bot.loadPlugin(pathfinder)
+
 bot.once('spawn', () => {
-    mineflayerViewer(bot, { port: 3000, version:"1.25.0" }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
+    mineflayerViewer(bot, { port: 3000, version:"1.25.0" ,firstPerson:true}) // port is the minecraft server port, if first person is false, you get a bird's-eye view
     const currentPosition = bot.entity.position;
     console.log(`Posición actual del bot: x=${currentPosition.x}, y=${currentPosition.y}, z=${currentPosition.z}`);
   })
 bot.on('chat', (username, message) => {
   if (username === bot.username) return
-  bot.chat(message)
+
+  if(message.startsWith("mine")){
+    let splitedMessge=message.split(" ")
+    
+    moveto(new GoalNear(parseInt(splitedMessge[1]), parseInt(splitedMessge[2]), parseInt(splitedMessge[3])),new Vec3( parseInt(splitedMessge[1]), parseInt(splitedMessge[2]),parseInt(splitedMessge[3])))
+  }
+  
 })
 
+function moveto(goal,pos){
+  const defaultMove = new Movements(bot)
+  bot.pathfinder.setMovements(defaultMove)
+  bot.pathfinder.setGoal(goal, RANGE_GOAL)
+  bot.once('goal_reached', () => {
+    bot.dig(targetBlock, onDigCompleted);
+  });
+  //mineBlock(1,pos)
+}
+
+
+
+async function mineBlock(blockType, position) {
+  console.log("f")
+  const targetBlock = bot.blockAt(position);
+  console.log(targetBlock.type)
+  if (targetBlock) {
+    try {
+      await bot.dig(targetBlock)
+      bot.chat(`finished digging ${targetBlock.name}`)
+    } catch (err) {
+      console.log(err.stack)
+    }
+  } else {
+    console.log('No se encontró el bloque objetivo.');
+  }
+}
+
+function onDigCompleted(err) {
+  if (err) {
+    console.error('Error al minar:', err);
+  } else {
+    console.log('Bloque minado exitosamente.');
+  }
+}
 
 // Log errors and kick reasons:
 bot.on('kicked', console.log)
