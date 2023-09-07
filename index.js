baa2d4bfbdb12c8d627236d559c1926bde3530d0
pelanguero <pelanguero@gmail.com>
2023-09-06 23:12:53 -0500

    prueba

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mineflayer = require('mineflayer')
 const mineflayerViewer = require('prismarine-viewer').mineflayer
-const { pathfinder, Movements, goals: { GoalNear } } = require('mineflayer-pathfinder')
+const { pathfinder, Movements, goals: { GoalNear, GoalPlaceBlock } } = require('mineflayer-pathfinder')
 const Vec3 = require('vec3');
 const RANGE_GOAL = 1
 const bot = mineflayer.createBot({
@@ -14,28 +14,44 @@ const bot = mineflayer.createBot({
 bot.loadPlugin(pathfinder)
 
 bot.once('spawn', () => {
-    mineflayerViewer(bot, { port: 3000, version:"1.25.0" ,firstPerson:true}) // port is the minecraft server port, if first person is false, you get a bird's-eye view
-    const currentPosition = bot.entity.position;
-    console.log(`Posición actual del bot: x=${currentPosition.x}, y=${currentPosition.y}, z=${currentPosition.z}`);
-  })
+  mineflayerViewer(bot, { port: 3000, version: "1.25.0", firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
+  const currentPosition = bot.entity.position;
+  mcData = require('minecraft-data')(bot.version);
+  console.log(`Posición actual del bot: x=${currentPosition.x}, y=${currentPosition.y}, z=${currentPosition.z}`);
+})
 bot.on('chat', (username, message) => {
   if (username === bot.username) return
 
-  if(message.startsWith("mine")){
-    let splitedMessge=message.split(" ")
-    
-    moveto(new GoalNear(parseInt(splitedMessge[1]), parseInt(splitedMessge[2]), parseInt(splitedMessge[3])),new Vec3( parseInt(splitedMessge[1]), parseInt(splitedMessge[2]),parseInt(splitedMessge[3])))
+  if (message.startsWith("mine")) {
+    let splitedMessge = message.split(" ")
+    let vectorr = new Vec3(parseInt(splitedMessge[1]), parseInt(splitedMessge[2]) + 1, parseInt(splitedMessge[3]))
+    moveto(new GoalPlaceBlock(vectorr, bot.world, options = { range: 1 }), new Vec3(parseInt(splitedMessge[1])+1, parseInt(splitedMessge[2])+1, parseInt(splitedMessge[3])))
   }
-  
+
 })
 
-function moveto(goal,pos){
+
+async function moveto(goal, pos) {
   const defaultMove = new Movements(bot)
   bot.pathfinder.setMovements(defaultMove)
   bot.pathfinder.setGoal(goal, RANGE_GOAL)
-  bot.once('goal_reached', () => {
-    bot.dig(targetBlock, onDigCompleted);
-  });
+  let moviendose = true
+  while (moviendose) {
+    console.log(bot.pathfinder.isMoving());
+    if (bot.pathfinder.isMoving()) {
+      await bot.equip(mcData.itemsByName["diamond_block"].id);
+      try {
+        let sourceBlock = bot.blockAt(pos);
+        let faceVector = { x: 0, y: 1, z: 0 };
+        await bot.placeBlock(sourceBlock, faceVector);
+        console.log("rere");
+      } catch (error) {
+        console.log(error);
+      }
+      moviendose=false
+    }
+  }
+  console.log("salio");
   //mineBlock(1,pos)
 }
 
